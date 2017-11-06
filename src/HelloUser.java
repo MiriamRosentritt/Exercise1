bfc06e216ca03673c088dc887d930cfc60fce76b
Miriam <Miri@192.168.188.23>
2017-11-06 21:20:30 +0100

    Initial commit
    Signed-off-by: Miriam <Miri@192.168.188.23>

---
+++ src/HelloUser.java
@@ -0,0 +1,12 @@
+
+public class HelloUser {
+	String userName;
+	
+	public HelloUser(String userName) {
+		this.userName = userName;
+	}
+	
+	public void greetUser() {
+		System.out.println("Hello "+ this.userName + "!");
+	}
+}
